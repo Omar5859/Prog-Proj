9b7028b1abd569df97ac7c171cb793b59fb70b78
Omar5859 <68243912+Omar5859@users.noreply.github.com>
2022-05-16 17:39:36 +0200

    Update SmplAssign.cpp

--- Statements/SmplAssign.cpp
+++ Statements/SmplAssign.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-SmplAssign::SmplAssign(Point Center, string LeftHS, double RightHS)
+SmplAssign::SmplAssign(Point Center, string LeftHS, double RightHS, ApplicationManager* app):Statement(Center)
 {
 	LHS = LeftHS;
 	RHS = RightHS;
@@ -11,17 +11,27 @@ SmplAssign::SmplAssign(Point Center, string LeftHS, double RightHS)
 	UpdateStatementText();
 
 	CenterPoint = Center;
-	
+
 	pConn = NULL;	//No connectors yet
+	pApp = app;
+
+	widthS = UI.ASSGN_WDTH;
+	heightS = UI.ASSGN_HI;
+
+	vStatX.first = CenterPoint.x - widthS / 2;
+	vStatX.second = CenterPoint.x + widthS / 2;
+
+	vStatY.first = CenterPoint.y - heightS / 2;
+	vStatY.second = CenterPoint.y + heightS / 2;
 
 	Inlet.x = CenterPoint.x;                        //Omar
-	Inlet.y = CenterPoint.y - UI.ASSGN_HI / 2;    
+	Inlet.y = CenterPoint.y - UI.ASSGN_HI / 2;
 
 	Outlet.x = Inlet.x;
-	Outlet.y = CenterPoint.y + UI.ASSGN_HI / 2;	
+	Outlet.y = CenterPoint.y + UI.ASSGN_HI / 2;
 }
 
-void SmplAssign::setLHS(const string &L)
+void SmplAssign::setLHS(const string& L)
 {
 	LHS = L;
 	UpdateStatementText();
@@ -36,22 +46,20 @@ void SmplAssign::setRHS(double R)
 
 void SmplAssign::Draw(Output* pOut) const
 {
-	//Call Output::DrawAssign function to draw assignment statement 	
 	pOut->DrawAssign(CenterPoint, UI.ASSGN_WDTH, UI.ASSGN_HI, Text, Selected);
-	
 }
 
 
 //This function should be called when LHS or RHS changes
 void SmplAssign::UpdateStatementText()
 {
-	if(LHS=="")	//No left handside ==>statement have no valid text yet
+	if (LHS == "")	//No left handside ==>statement have no valid text yet
 		Text = "    = ";
 	else
 	{
 		//Build the statement text: Left handside then equals then right handside
 		ostringstream T;
-		T<<LHS<<" = "<<RHS;	
-		Text = T.str();	 
+		T << LHS << " = " << RHS;
+		Text = T.str();
 	}
 }
