539d5e8fab06c7a6e01f60927d5cde0b6ebc248d
Omar5859 <68243912+Omar5859@users.noreply.github.com>
2022-05-16 17:31:55 +0200

    Update ApplicationManager.h

--- Application manager/ApplicationManager.h
+++ Application manager/ApplicationManager.h
@@ -2,11 +2,10 @@
 #define APPLICATION_MANAGER_H
 
 #include "DEFS.h"
-
-#include "Statements\Statement.h"
-class Input;
-class Output;
-
+#include "Statements/Statement.h"
+#include "Statements/Connector.h"
+#include "GUI/Input.h"
+#include "GUI/Output.h"
 
 //Main class that manages everything in the application.
 class ApplicationManager
@@ -19,38 +18,50 @@ private:
 	Statement* StatList[MaxCount];	//List of all statements (Array of pointers)
 	Connector* ConnList[MaxCount];	//List of all connectors (Array of pointers)
 
-	Statement *pSelectedStat; //a pointer to the last selected statement
+	Statement* pSelectedStat; //a pointer to the last selected statement
+	Connector* pSelectedConn;
+
+	pair<int, int>* ArrX;
+	pair<int, int>* ArrY;
 
 	//Pointers to Input and Output classes
-	Input *pIn;
-	Output *pOut;
+	Input* pIn;
+	Output* pOut;
 
-public:	
-	ApplicationManager(); 
+public:
+	ApplicationManager();
 	~ApplicationManager();
-	
+
 	// -- Actions Related Functions
 	//Reads the input command from the user and returns the corresponding action type
 	ActionType GetUserAction() const;
-	void ExecuteAction(ActionType) ; //Creates an action and executes it
-	
+	void ExecuteAction(ActionType); //Creates an action and executes it
+
 	// -- Statements/Connector Management Functions
 	void AddStatement(Statement* pStat); //Adds a new Statement to the Flowchart
-	Statement *GetStatement(Point P) const;	//search for a statement where point P belongs
+	Statement* GetStatement(Point P) const;	//search for a statement where point P belongs
 
-	void AddConnector(Connector* pConn); //Adds a new Connector to the Flowchart
-	Connector *GetConnector(Point P) const;	//search for a Connector where point P belongs
+	Statement* & GetSelectedStatement();	//Returns the selected Statement
+	void SetSelectedStatement(Statement* pStat); //Set the Statement selected by the user
 
+	void AddConnector(Connector* pConn); //Adds a new Connector to the flowchart
+	Connector* GetConnector(Point P) const; //Search for a statement where point p belongs
 
-	Statement *GetSelectedStatement() const;	//Returns the selected Statement
-	void SetSelectedStatement(Statement *pStat); //Set the Statement selected by the user
+	void SetSelectedConnector(Connector* pConn); //Set the Connector Statement by the user
+	Connector* & GetSelectedConnector(); //Returns the selected Connector
 
-		
+	int GetStatCount();
+	int GetConnCount();
+	Statement** GetStatList();    //Returns each statement (Pointer-Pointer-Object)
+	Connector** GetConnList();
+
+	//bool CheckIntersection(ApplicationManager* pApp, pair<int,int> PX, pair<int,int> PY, int width, int height, int id);
+
 	// -- Interface Management Functions
-	Input *GetInput() const; //Return pointer to the input
-	Output *GetOutput() const; //Return pointer to the output
+	Input* GetInput() const; //Return pointer to the input
+	Output* GetOutput() const; //Return pointer to the output
 	void UpdateInterface() const;	//Redraws all the drawing window
-	
+
 };
 
 #endif
