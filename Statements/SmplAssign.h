bb72dd76bc557dc5824a5d0a00d0bfc7286c85c5
Omar5859 <68243912+Omar5859@users.noreply.github.com>
2022-05-16 17:38:00 +0200

    Update SmplAssign.h

--- Statements/SmplAssign.h
+++ Statements/SmplAssign.h
@@ -2,6 +2,7 @@
 #define SMPL_ASSIGN_H
 
 #include "Statement.h"
+#include "..\ApplicationManager.h"
 
 //Simple Assignment statement class
 //The simple assignment statement assigns a variable to a value
@@ -10,20 +11,19 @@ class SmplAssign : public Statement
 private:
 	string LHS;	//Left Handside of the assignment (name of a variable)
 	double RHS;	//Right Handside (Value)
-	
-	Connector *pConn;	//Simple Assignment Stat. has one Connector to next statement
+
+	Connector* pConn;	//Simple Assignment Stat. has one Connector to next statement
+	ApplicationManager* pApp;
 
 	Point Inlet;	//A point where connections enters this statement 
 	Point Outlet;	//A point a connection leaves this statement
 
-	Point CenterPoint;	//Center of the statement block.
-
 	virtual void UpdateStatementText();
-	
+
 public:
-	SmplAssign(Point Center, string LeftHS="", double RightHS=0);
-	
-	void setLHS(const string &L);
+	SmplAssign(Point Center, string LeftHS = "", double RightHS = 0, ApplicationManager* app = NULL);
+
+	void setLHS(const string& L);
 	void setRHS(double R);
 
 	virtual void Draw(Output* pOut) const;
